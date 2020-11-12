2575312da24b5c7f91b33161097df89722b02b17
Topiga <topiga.frt@gmail.com>
2020-11-12 20:25:19 +0100

    It should work now ?

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ tar xf apk-tools-static-2.10.5-r1.apk sbin/apk.static
 ./sbin/apk.static add apk-tools
 rm -r sbin
 apk add bash openssl openssh sudo nano mandoc man-pages less less-doc
-cat /etc/passwd | tail -n $(($(cat /etc/passwd | wc -l)-1)) > tmp_passwd && echo 'root:x:0:0:root:/bin/bash' > /etc/passwd && cat tmp_passwd >> /etc/passwd && rm -f tmp_passwd
+cat /etc/passwd | tail -n $(($(cat /etc/passwd | wc -l)-1)) > tmp_passwd && echo 'root:x:0:0:root:/root:/bin/ash' > /etc/passwd && cat tmp_passwd >> /etc/passwd && rm -f tmp_passwd
 sleep 1
 cat << EOF > ~/.bash_profile
 export PS1="\[\033[01;31m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
